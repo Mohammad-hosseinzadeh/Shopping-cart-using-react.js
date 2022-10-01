ca18aa722e9a82bffea65b9bc3765f71786eec4c
Mohammad-hosseinzadeh <webhosseinzadeh@gmail.com>
2022-10-01 10:35:51 +0330

    add getProducts function

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const BASE_URL = "https://fakestoreapi.com";
 // get products data 
-const getProducts = async () => {
+const getProducts=async()=>{
     const response = await axios.get(`${BASE_URL}/products`);
     return response;
-};
-export { getProducts };
+}
+export {getProducts}
