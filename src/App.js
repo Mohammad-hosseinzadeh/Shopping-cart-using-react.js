dc6e8d67087c7fae36ca55fd884e6f0b93233e7a
Mohammad-hosseinzadeh <webhosseinzadeh@gmail.com>
2022-10-01 18:15:11 +0330

    Store routing with react-route-v5

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 // components
 import Store from "./components/Store";
 
+import { Switch,Route,Redirect } from "react-router-dom";
+
 // context
 import ProductContextProvider from "./context/ProductContextProvider";
 
 export default function App() {
     return(
      <ProductContextProvider>
-        <Store/>
+        <Switch>
+            <Route path="/" component={Store}/>
+            <Redirect to="/products"/>
+        </Switch>
+        
      </ProductContextProvider>
     );
 }
