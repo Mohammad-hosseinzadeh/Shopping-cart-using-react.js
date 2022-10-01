12ae481ab2dc93ff403a6d01132a85a721661602
Mohammad-hosseinzadeh <webhosseinzadeh@gmail.com>
2022-10-01 16:53:08 +0330

    create Product component and using map in Store component on Product

---
+++ src/Shared/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div>
+        <img/>
+        <h3>Title</h3>
+        <p>price</p>
+        <div>
+            <a>Detailes</a>
+            <div>
+                <button>
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,9 +1,15 @@
-import React,{useContext} from 'react'
+import React,{useContext} from 'react';
+// component
+import Product from '../Shared/Product';
 // context
 import { ProductsContext } from '../context/ProductContextProvider'
 export default function Store() {
     const products=useContext(ProductsContext)
   return (
-    <div>Store</div>
+    <div>
+        {
+            products.map(product=><Product key={product.id} productData={product}/>)
+        }
+    </div>
   )
 }
